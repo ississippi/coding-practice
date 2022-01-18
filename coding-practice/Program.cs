25032376a88a45ed2d762665cb7ada602da44f79
Gary Smith <ississippi@outlook.com>
2022-01-18 07:30:37 -0500

    Add project files.

---
+++ coding-practice/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using coding_practice;
+
+var a = new char[] { 'h', 'e', 'l', 'l', 'o' }; 
+
+var s1 = new ReverseString();
+s1.ReverseString2Pointers(a);
+Console.WriteLine(a);
+s1.ReverseStringRecursive(a);
+Console.WriteLine(a);
+
+
+
+
+
---
+++ coding-practice/ReverseString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_practice
+{
+    internal class ReverseString
+    {
+        public void ReverseString2Pointers(char[] s)
+        {
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left++] = s[right];
+                s[right--] = tmp;
+            }
+        }
+
+        public void helper(char[] s, int left, int right)
+        {
+            if (left >= right) return;
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+            helper(s, left, right);
+        }
+
+        public void ReverseStringRecursive(char[] s)
+        {
+            helper(s, 0, s.Length - 1);
+        }
+    }
+}
+
+
