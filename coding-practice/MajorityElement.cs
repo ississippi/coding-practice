48323d30547fe75b89bd9c35250513550666b1a7
Gary Smith <ississippi@outlook.com>
2022-01-29 12:56:50 -0500

    MajorityElement

---
+++ coding-practice/MajorityElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_practice
+{
+    internal class MajorityElementClass
+    {
+        public int MajorityElement(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+            var me = 0;
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (d.ContainsKey(nums[i]))
+                {
+                    d[nums[i]]++;
+                }
+                else
+                {
+                    d.Add(nums[i], 1);    
+                }
+            }
+            var max = 0;
+            foreach(var item in d)
+            {
+                if(item.Value > max)
+                {
+                    me = item.Key;  
+                    max = item.Value;   
+                }
+            }
+            return me;
+        }
+
+    }
+}
--- coding-practice/Program.cs
+++ coding-practice/Program.cs
@@ -93,26 +93,37 @@ var d = new int[] { 4, 1, 2, 1, 2 };
 // ======================
 // Excel Columns
 // ======================
-var x = new ExcelColumns();
-var sum = x.TitleToNumber("A");
-Console.WriteLine(sum); 
-sum = x.TitleToNumber("AB");
-Console.WriteLine(sum);
-sum = x.TitleToNumber("AAA");
-Console.WriteLine(sum);
-sum = x.TitleToNumber("FXSHRXW"); //2147483647
-Console.WriteLine(sum);
-sum = x.TitleToNumber("F"); //2147483647
-Console.WriteLine(sum);
-sum = x.TitleToNumber("X"); //2147483647
-Console.WriteLine(sum*26);
-sum = x.TitleToNumber("S"); //2147483647
-Console.WriteLine(sum * 26);
-sum = x.TitleToNumber("H"); //2147483647
-Console.WriteLine(sum * 26);
-sum = x.TitleToNumber("R"); //2147483647
-Console.WriteLine(sum * 26);
-sum = x.TitleToNumber("X"); //2147483647
-Console.WriteLine(sum * 26);
-sum = x.TitleToNumber("W"); //2147483647
-Console.WriteLine(sum * 26);
+//var x = new ExcelColumns();
+//var sum = x.TitleToNumber("A");
+//Console.WriteLine(sum); 
+//sum = x.TitleToNumber("AB");
+//Console.WriteLine(sum);
+//sum = x.TitleToNumber("AAA");
+//Console.WriteLine(sum);
+//sum = x.TitleToNumber("FXSHRXW"); //2147483647
+//Console.WriteLine(sum);
+//sum = x.TitleToNumber("F"); //2147483647
+//Console.WriteLine(sum);
+//sum = x.TitleToNumber("X"); //2147483647
+//Console.WriteLine(sum*26);
+//sum = x.TitleToNumber("S"); //2147483647
+//Console.WriteLine(sum * 26);
+//sum = x.TitleToNumber("H"); //2147483647
+//Console.WriteLine(sum * 26);
+//sum = x.TitleToNumber("R"); //2147483647
+//Console.WriteLine(sum * 26);
+//sum = x.TitleToNumber("X"); //2147483647
+//Console.WriteLine(sum * 26);
+//sum = x.TitleToNumber("W"); //2147483647
+//Console.WriteLine(sum * 26);
+
+// ======================
+// Majority Element
+// ======================
+var mec = new MajorityElementClass();
+int[] meArr2 = new int[] { 1, 2, 3, 2, 4, 1, 3, 5, 6, 7, 7, 5, 7};
+int[] meArr1 = new int[] { 3, 2, 3 };
+int[] meArr3 = new int[] {8,8,7,7,7};
+int[] meArr4 = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+var me = mec.MajorityElement(meArr4);
+Console.WriteLine(me);
